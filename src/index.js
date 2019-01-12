a769089ff009703cc0d622b49a0958a796ab112b
superman <superman.codegiant@gmail.com>
2019-01-12 14:23:08 +0530

    Removed provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import { Provider } from 'react-redux'
-import { applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 // import { createStore, applyMiddleware, compose } from 'redux'
 
 // const store = createStore(rootReducer, compose(composeEnhancers))
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
